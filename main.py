ea082cb811fc780d2002e7677628bc14c0311b0e
szmn-git <informatyczne123@gmail.com>
2021-07-29 11:15:40 +0200

    check conditions - [def, list of Booleans]

--- main.py
+++ main.py
@@ -7,12 +7,21 @@ def generate(lista):
         password += lista[random.randrange(0, len(lista))]
     return password
 
+def checkCond(pasw):
+    lista = [bool(any(letter in pasw for letter in letters)), bool(any(letter in pasw for letter in uppers)),
+             bool(any(letter in pasw for letter in numbers)), bool(any(letter in pasw for letter in special))]
+    return lista
+
+
+letters = list(string.ascii_lowercase)
 uppers = list(string.ascii_uppercase)
 numbers = list('0123456789')
 special = list('!@#$%^&*()?')
 
-lista = list(string.ascii_lowercase) + uppers + numbers + special
+lista = letters + uppers + numbers + special
 
 ans = generate(lista)
 
-print(ans)
+temp = False
+
+print(checkCond(ans))
